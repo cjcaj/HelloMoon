eaa388c07d4af7c8362923ad59e041282a879a1f
carlo <carlo@ichange.com>
2014-03-11 11:40:34 -0700

    Finish Chapter 13

---
+++ src/com/cjcaj/android/HelloMoon/HelloMoonActivity.java
@@ -0,0 +1,16 @@
+package com.cjcaj.android.HelloMoon;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class HelloMoonActivity extends FragmentActivity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_moon);
+    }
+}
