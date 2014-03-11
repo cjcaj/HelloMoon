3b54df5226d79c3c733d7cafa8404f8d69477281
carlo <carlo@ichange.com>
2014-03-11 15:49:27 -0700

    Add video player

--- src/com/cjcaj/android/HelloMoon/HelloMoonFragment.java
+++ src/com/cjcaj/android/HelloMoon/HelloMoonFragment.java
@@ -1,11 +1,14 @@
 package com.cjcaj.android.HelloMoon;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 /**
  * Created by carloca on 3/11/14.
@@ -15,35 +18,19 @@ public class HelloMoonFragment extends Fragment {
     private Button mPlayButton;
     private Button mStopButton;
     private Button mPauseButton;
+    private VideoView mVideoView;
+    private MediaController mMediaController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
 
-        mPlayButton = (Button) v.findViewById(R.id.hellomoon_playButton);
-        mPlayButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                mPlayer.play(getActivity());
-            }
-        });
-        mStopButton = (Button) v.findViewById(R.id.hellomoon_stopButton);
-        mStopButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                mPlayer.stop();
-            }
-        });
-        mPauseButton = (Button) v.findViewById(R.id.hellomoon_pauseButton);
-        mPauseButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                mPlayer.pause();
-            }
-        });
+        mVideoView = (VideoView) v.findViewById(R.id.hellomoon_videoView);
+        mMediaController = new MediaController(getActivity());
+        Uri resourceUri = Uri.parse("android.resource://com.cjcaj.android.HelloMoon/raw/apollo_17_stroll");
+        mVideoView.setVideoURI(resourceUri);
+        mVideoView.setMediaController(mMediaController);
+        mVideoView.start();
         return v;
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mPlayer.stop();
     }
 }
